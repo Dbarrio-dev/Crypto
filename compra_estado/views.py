f015caaaf5cd5002e10039ccd7342c20757a898d
Daniel <d.barriele@gmail.com>
2021-01-20 19:02:42 +0100

    Repo actualizado

--- compra_estado/views.py
+++ compra_estado/views.py
@@ -4,12 +4,41 @@ from flask import render_template, request, url_for
 
 import sqlite3
 
+from requests import Request, Session
+
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+
+import json
+
+
+url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+parameters = {
+'start':'1',
+'limit':'5000',
+'convert':'USD'
+}
+headers = {
+'Accepts': 'application/json',
+'X-CMC_PRO_API_KEY': 'f4306891-4e3c-4072-98d0-3a649dd0c2f4',
+}
+
+session = Session()
+session.headers.update(headers)
+
+try:
+    response = session.get(url, params=parameters)
+    data = json.loads(response.text)
+except (ConnectionError, Timeout, TooManyRedirects) as e:
+    print(e)
+
 @app.route('/')
 def tabla_incio():
+
     return render_template("incio.html")
 
-@app.route('/purchase')
+@app.route('/purchase', methods=['GET','POST'])
 def interaccion():
+
     return render_template ("purchase.html")
 
 @app.route('/status')  
